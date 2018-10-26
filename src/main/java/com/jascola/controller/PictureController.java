89bbe8ee66d810ab8c38b93f811d4f6cd759d211
jascola <1947857236@qq.com>
2018-10-26 10:01:15 +0800

    修改文件上传为异步

--- src/main/java/com/jascola/controller/PictureController.java
+++ src/main/java/com/jascola/controller/PictureController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -46,13 +48,39 @@ public class PictureController extends BaseController {
 
 
     @RequestMapping(value = "/upload.html")
-    public void upload(HttpServletResponse response, PictureDto formDate, HttpServletRequest request) {
+    @ResponseBody
+    public DeferredResult<String> aysncUpload(HttpServletResponse response, PictureDto formDate, HttpServletRequest request) throws InterruptedException {
+        DeferredResult<String> deferredResult = new DeferredResult<String>();
+        deferredResult = upload(formDate,deferredResult);
+        return deferredResult;
+    }
+    @RequestMapping(value = "deletepic.html")
+    public void deletePic(String id,HttpServletResponse response,HttpServletRequest request){
         List<String> messages = new ArrayList<String>();
         Jedis jedis = jedisPool.getResource();
         /*String content = super.tokenAdminCheck(response, request, messages, jedisPool);
         if (content == null) {
             return;
         }*/
+        try{
+            pictureservice.deleteById(id);
+            messages.add("删除相册成功");
+            for(int i=0;i<6;i++) {
+                jedis.select(i);
+                jedis.flushDB();
+            }
+            super.ResponseSuccess(response,messages);
+        }catch (Exception e){
+            messages.add("删除相册失败");
+            super.ResponseError(response,messages);
+            LOGGER.error(e.getLocalizedMessage(),e);
+        }finally {
+            jedis.close();
+        }
+    }
+    private DeferredResult<String> upload(PictureDto formDate, DeferredResult<String> deferredResult) throws InterruptedException {
+        Thread.sleep(20000);
+        Jedis jedis = jedisPool.getResource();
         MultipartFile[] files = formDate.getImages();
         PicturesEntity entity = new PicturesEntity();
         MultipartFile file = formDate.getImage();
@@ -78,7 +106,6 @@ public class PictureController extends BaseController {
             if (!filex.exists()) {
                 filex.mkdir();
             }
-
 
             String fileName = file.getOriginalFilename(); //获得文件名称
 
@@ -112,43 +139,18 @@ public class PictureController extends BaseController {
                         jedis.flushDB();
                     }
                 }
-                messages.add("上传成功");
-                super.ResponseSuccess(response, messages);
-                return;
+                deferredResult.setResult("0");
+                return deferredResult;
             } catch (Exception e) {
-                messages.add("上传失败!");
-                super.ResponseError(response, messages);
+                deferredResult.setResult("1");
                 LOGGER.error(e.getLocalizedMessage(), e);
-                return;
+                return deferredResult;
             }finally {
                 jedis.close();
             }
         }
-        messages.add("文件不能为空或重复！");
-        super.ResponseError(response, messages);
+        deferredResult.setResult("2");
+        return deferredResult;
     }
-    @RequestMapping(value = "deletepic.html")
-    public void deletePic(String id,HttpServletResponse response,HttpServletRequest request){
-        List<String> messages = new ArrayList<String>();
-        Jedis jedis = jedisPool.getResource();
-        /*String content = super.tokenAdminCheck(response, request, messages, jedisPool);
-        if (content == null) {
-            return;
-        }*/
-        try{
-            pictureservice.deleteById(id);
-            messages.add("删除相册成功");
-            for(int i=0;i<6;i++) {
-                jedis.select(i);
-                jedis.flushDB();
-            }
-            super.ResponseSuccess(response,messages);
-        }catch (Exception e){
-            messages.add("删除相册失败");
-            super.ResponseError(response,messages);
-            LOGGER.error(e.getLocalizedMessage(),e);
-        }finally {
-            jedis.close();
-        }
-    }
+
 }
