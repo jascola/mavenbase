f10d6192cd1aebb795ec4a601d2f5c12919fca97
jascola <1947857236@qq.com>
2018-11-28 15:24:59 +0800

    sqlMap

--- src/main/java/com/jascola/dao/AcgDao.java
+++ src/main/java/com/jascola/dao/AcgDao.java
@@ -1,10 +1,12 @@
 package com.jascola.dao;
 
+import com.jascola.entity.AcgEntity;
 import org.springframework.stereotype.Repository;
-
 
 @Repository
 public interface AcgDao {
 
+    int insert(AcgEntity entity);
+    int update(AcgEntity entity);
 
 }
--- src/main/java/com/jascola/service/AcgService.java
+++ src/main/java/com/jascola/service/AcgService.java
@@ -2,6 +2,7 @@ package com.jascola.service;
 
 
 import com.jascola.dao.AcgDao;
+import com.jascola.entity.AcgEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,11 @@ import org.springframework.stereotype.Service;
 public class AcgService {
     @Autowired
     private AcgDao dao;
-
+    public int insert(AcgEntity entity){
+        return dao.insert(entity);
+    }
+    public int update(AcgEntity entity){
+        return dao.update(entity);
+    }
 
 }
